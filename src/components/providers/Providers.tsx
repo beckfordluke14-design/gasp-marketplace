c4f932d0b1d8d518833322a344680cb18c435fc0
beckfordluke14-design <beckfordluke14@gmail.com>
2026-03-31 08:28:59 -0400

    Update Providers.tsx

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,33 +1,14 @@
 'use client';
 
-import { PrivyProvider } from "@privy-io/react-auth";
+import { GaspPrivyProvider } from '@/components/providers/GaspPrivyProvider';
 import { UserProvider } from '@/components/providers/UserProvider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <PrivyProvider
-      appId="cm1is5rpg00is4qonf00ndkki"
-      config={{
-        loginMethods: ['google', 'twitter', 'discord', 'email', 'wallet'],
-        appearance: {
-          theme: 'dark',
-          accentColor: '#ff00ff',
-          showWalletLoginFirst: false,
-          logo: 'https://asset.gasp.fun/logo.png',
-        },
-        embeddedWallets: {
-          ethereum: {
-            createOnLogin: 'users-without-wallets',
-          },
-          solana: {
-            createOnLogin: 'users-without-wallets',
-          },
-        },
-      }}
-    >
+    <GaspPrivyProvider>
       <UserProvider>
         {children}
       </UserProvider>
-    </PrivyProvider>
+    </GaspPrivyProvider>
   );
 }
